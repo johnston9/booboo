56233fcee5decae5317268b7810883465d0f531f
johnston9 <davejohnston9@gmail.com>
2021-05-12 22:26:16 +0000

    stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IqMAeBdsZDTSd1kHkIzhILstCW8vsDxsvYRKaxSKNlYUEz5FnzlQHRRaWp8YujjfzuSky5MQrgfiiIThOKX71uQ009hGm0xQg',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
