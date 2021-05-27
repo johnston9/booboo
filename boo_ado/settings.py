829e2523d3cd7c405cfe70dfea396092713defee
johnston9 <davejohnston9@gmail.com>
2021-05-27 20:07:27 +0000

    secret key and debug

--- boo_ado/settings.py
+++ boo_ado/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-69(e5v-z0f@f+=-84048j%vqqnxv6-pf^49ledp7k+998tv-sw'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boo-ado.herokuapp.com', 'localhost']
 
